940f6eb6a793a56c46f08574085723aec9e325df
Om Patil <omajitsinhpatil@gmail.com>
2023-02-23 16:24:01 +0530

    login screen added

--- app/src/main/java/com/example/mindsparkui/MainActivity.java
+++ app/src/main/java/com/example/mindsparkui/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.mindsparkui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText username;
+    EditText password;
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void startScreen(View view) {
+        username=findViewById(R.id.username);
+        password=findViewById(R.id.password);
+        loginButton =findViewById(R.id.loginButton);
+        if(username.getText().toString().equals("12345") && password.getText().toString().equals("12345")){
+            Intent intent = new Intent(MainActivity.this, events.class);
+            startActivity(intent);
+        }else{
+            Toast.makeText(this, "Invalid login credentials", Toast.LENGTH_SHORT).show();
+        }
+
+//        Intent i=new Intent();
     }
 }
