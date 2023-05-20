3aa7133c9d675ccf5d7cc6146442dc0dd9d94b87
Om Patil <omajitsinhpatil@gmail.com>
2023-05-20 15:08:29 +0530

    drawer button in logout*

--- app/src/main/java/com/example/mindsparkui/events.java
+++ app/src/main/java/com/example/mindsparkui/events.java
@@ -6,7 +6,26 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.MenuItem;
 import android.view.View;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
+
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.mindsparkui.databinding.ActivityEventsBinding;
 import android.view.View;
 import android.view.Menu;
 import android.widget.Button;
@@ -55,20 +74,39 @@ public class events extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityEventsBinding binding;
-//    private ViewPager viewPager;
-//    private LinearLayout circleIndicatorLayout;
-//    private int[] imageResources = {R.drawable.msone, R.drawable.mstwo, R.drawable.msthree};
-//    private int currentPage = 0;
+    private ViewPager viewPager;
+    private LinearLayout circleIndicatorLayout;
+    private int[] imageResources = {R.drawable.msone, R.drawable.mstwo, R.drawable.msthree};
+    private int currentPage = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         binding = ActivityEventsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
+        setSupportActionBar(binding.appBarEvents.toolbar);
+        binding.appBarEvents.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Contact Developer: omajitsinhpatil@gmail.com", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        DrawerLayout drawer = binding.drawerLayout;
+        NavigationView navigationView = binding.navView;
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                .setOpenableLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_events);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
         // Your other code
 
-        NavigationView navigationView = binding.navView;
+         navigationView = binding.navView;
         View headerView = navigationView.getHeaderView(0);
 
         ImageView imageView = headerView.findViewById(R.id.imageView);
